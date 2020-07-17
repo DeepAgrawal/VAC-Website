db573f951830e5eab1886662d4179017ee23b36d
Deep Agrawal <deepsanjay.agrawal2018@vitstudent.ac.in>
2020-07-17 18:18:31 +0530

    theme color changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,8 @@ module.exports = {
         name: `VIT Animation Club`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
