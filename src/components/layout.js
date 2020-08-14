0818d1a11b3dd0331923c5187ff8750f11ac420f
Deep Agrawal <deepsanjay.agrawal2018@vitstudent.ac.in>
2020-08-14 17:15:22 +0530

    schema

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,9 +13,9 @@ const Layout = ({ children }) => {
       <Helmet>
         <script type="application/ld+json">
           "@context": "https://schema.org", "@type": "Organization", "name":
-          "VIT Animation Club", "url": "http://vitanimationclub.tech", "logo":
-          "https://vitanimationclub.tech/static/a6a87e6874dbb825c326d357c1b2a5dc/3b869/oglogo.png",
-          "sameAs": [ "https://www.facebook.com/vitanimationclub",
+          "VIT Animation Club", "url": "http://vitanimationclub.tech", "url" :
+          "http://vitanimationclub.tech", "sameAs": [
+          "https://www.facebook.com/vitanimationclub",
           "https://www.instagram.com/vit_animation/",
           "https://www.youtube.com/channel/UCv6jdaXdcxnaoqKvmFC3epw",
           "https://www.linkedin.com/company/vit-animation" ]
