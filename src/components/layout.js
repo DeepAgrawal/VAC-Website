e847ce6a5ebf5d9b8410bf47a040220417d7cc3a
Deep Agrawal <deepsanjay.agrawal2018@vitstudent.ac.in>
2020-08-14 17:08:34 +0530

    Schema script Added 📃

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
 
 import "../styles/App.scss"
 import Contact from "../components/Contact"
@@ -9,6 +10,17 @@ const Layout = ({ children }) => {
     <>
       <main>{children}</main>
       <Contact />
+      <Helmet>
+        <script type="application/ld+json">
+          "@context": "https://schema.org", "@type": "Organization", "name":
+          "VIT Animation Club", "url": "http://vitanimationclub.tech", "logo":
+          "https://vitanimationclub.tech/static/a6a87e6874dbb825c326d357c1b2a5dc/3b869/oglogo.png",
+          "sameAs": [ "https://www.facebook.com/vitanimationclub",
+          "https://www.instagram.com/vit_animation/",
+          "https://www.youtube.com/channel/UCv6jdaXdcxnaoqKvmFC3epw",
+          "https://www.linkedin.com/company/vit-animation" ]
+        </script>
+      </Helmet>
     </>
   )
 }
--- src/components/seo.js
+++ src/components/seo.js
@@ -114,17 +114,7 @@ function SEO({ description, lang, meta, title, pathname }) {
         name: "keywords",
         content: meta.join(", "),
       })}
-    >
-      <script type="application/ld+json">
-        "@context": "https://schema.org", "@type": "Organization", "name": "VIT
-        Animation Club", "url": "http://vitanimationclub.tech", "logo":
-        "https://vitanimationclub.tech/static/a6a87e6874dbb825c326d357c1b2a5dc/3b869/oglogo.png",
-        "sameAs": [ "https://www.facebook.com/vitanimationclub",
-        "https://www.instagram.com/vit_animation/",
-        "https://www.youtube.com/channel/UCv6jdaXdcxnaoqKvmFC3epw",
-        "https://www.linkedin.com/company/vit-animation" ]
-      </script>
-    </Helmet>
+    />
   )
 }
 // meta && meta.length > 0
